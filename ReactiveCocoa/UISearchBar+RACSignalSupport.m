50616cdc66a4410ad385a591d42ffbdc12d478c6
Michael-LFX <cihv2@163.com>
2015-03-29 21:07:47 +0800

    add distinctUntilChanged

--- ReactiveCocoa/UISearchBar+RACSignalSupport.m
+++ ReactiveCocoa/UISearchBar+RACSignalSupport.m
@@ -10,11 +10,12 @@
     if (signal) {
         return signal;
     }
-    signal = [[[self rac_signalForSelector:@selector(searchBar:textDidChange:)
-                              fromProtocol:@protocol(UISearchBarDelegate)]
-               map:^id(RACTuple *tuple) {
-                   return tuple.second;
-               }]
+    signal = [[[[self rac_signalForSelector:@selector(searchBar:textDidChange:)
+                               fromProtocol:@protocol(UISearchBarDelegate)]
+                map:^id(RACTuple *tuple) {
+                    return tuple.second;
+                }]
+               distinctUntilChanged]
               setNameWithFormat:@"%@ -rac_textSignal", self.rac_description];
     objc_setAssociatedObject(self, _cmd, signal, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     return signal;
